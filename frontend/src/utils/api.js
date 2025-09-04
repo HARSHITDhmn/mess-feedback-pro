2cb76c99a7555a6f564abbf2ec4c29ffe861a17b
Harshit <160234639+HARSHITDhmn@users.noreply.github.com>
2025-09-05 01:13:31 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 const api = axios.create({ baseURL: 'https://mess-feedback-pro-3.onrender.com' });
-api.interceptors.request.use(cfg=>{
-  const token = localStorage.getItem('token');
-  if(token) cfg.headers.Authorization = 'Bearer ' + token;
-  return cfg;
-});
 export default api;
 
 
