633062021710e419de011ab080e8dc77b6b9cf26
Harshit <160234639+HARSHITDhmn@users.noreply.github.com>
2025-09-05 01:22:07 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'https://mess-feedback-pro-3.onrender.com' });
+const api = axios.create({ baseURL: 'https://mess-feedback-pro-3.onrender.com'  });
 export default api;
 
 
